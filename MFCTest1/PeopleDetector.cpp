5c448f5ffbad3a806edc84af1c07124b04b5519e
freedomDR <ZJRforeverDR@gmail.com>
2017-05-17 10:44:21 +0800

    基本完成

--- MFCTest1/PeopleDetector.cpp
+++ MFCTest1/PeopleDetector.cpp
@@ -8,6 +8,7 @@
 
 PeopleDetector::PeopleDetector()
 {
+	found_filtered_flag = true;
 }
 
 
@@ -26,13 +27,8 @@ void PeopleDetector::process2(VideoCapture &caputure, LPVOID params)
 		if (!caputure.read(img)) return;
 		Mat img_copy;
 		img.copyTo(img_copy);
-		HOGDescriptor hog;  //HOG特征描述子
-		hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());  //得到SVM检测算子----用于对hog特征进行分类的svm模型的系数赋值
-
-		vector<Rect> found, found_filtered;
+		
 		cvtColor(img_copy, img_copy, CV_BGR2GRAY);
-		//imshow("img_copy", img_copy);
-		//int can = img.channels();  //获取通道数
 
 		/*Size win_stride=Size(), Size padding=Size(), double scale0=1.05, int group_threshold=2)
         功能：对输入的图片img进行多尺度的行人检测.
@@ -44,32 +40,45 @@ void PeopleDetector::process2(VideoCapture &caputure, LPVOID params)
 		scale0：比例系数，即滑动窗口每次增加的比例
 		group_threshold：校正系数，当一个目标被多个窗口检测出来时，起调节作用.设置为0是不调节*/
 
-		hog.detectMultiScale(img_copy, found, 0, Size(8, 8), Size(0, 0), 1.5, 2);
-
 		
-		size_t i, j;
-		//找出没有嵌套的矩形框r，如果有嵌套的话，保留大的矩形框
-		for (i = 0; i < found.size(); i++)
+		this->img = img_copy;
+		if (this_back->frame_order % 10 == 0)
 		{
-			Rect r = found[i];
-			for (j = 0; j < found.size(); j++)
-				if (j != i && (r & found[j]) == r)
-					break;
-				if (j == found.size())
-					found_filtered.push_back(r);
+			//found_filtered_flag = false;
+			//found.clear();
+			//found_filtered.clear();
+			AfxBeginThread(DealPeople, this, 0, 0, 0, 0);
+			
 		}
 
+		
+		
+
 		//画矩形框
-		for (i = 0; i < found_filtered.size(); i++)
+		if (found_filtered_flag == true)
 		{
-			Rect r = found_filtered[i];
-			// 因为HOG detector返回的事比目标大的矩形框，因此应缩小矩形框
-			r.x += cvRound(r.width*0.1);
-			r.width = cvRound(r.width*0.8);
-			r.y += cvRound(r.height*0.07);
-			r.height = cvRound(r.height*0.8);
-			rectangle(img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);  //img-图片，tl----the top-left corner，br----the top-left corner，Scalar()-矩形框颜色，config6-线宽
-				
+			int i;
+			BOOL flag_mid = flag;
+			for (i = 0; i < found_filtered.size(); i++)
+			{
+				Rect r = found_filtered[i];
+				// 因为HOG detector返回的事比目标大的矩形框，因此应缩小矩形框
+				r.x += cvRound(r.width*0.1);
+				r.width = cvRound(r.width*0.8);
+				r.y += cvRound(r.height*0.07);
+				r.height = cvRound(r.height*0.8);
+				rectangle(img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);  //img-图片，tl----the top-left corner，br----the top-left corner，Scalar()-矩形框颜色，config6-线宽
+				if (r.y+r.height >= initEnd && flag_mid == true)
+				{
+					flag_mid = false;
+					AfxBeginThread(NotifyPeople, this, 0, 0, 0, 0);
+					//MessageBeep(MB_OK);
+					//Beep(5000, 500);
+					//MessageBeep(MB_ICONHAND);
+					//Sleep(3000);
+					
+				}
+			}
 		}
 		resize(img, img, Size(this_back->picture_y, this_back->picture_x));
 	    if(this_back->peopleDetector.flag==true)
@@ -100,7 +109,42 @@ void PeopleDetector::onMouseAction(int event, int x, int y, int flags, void * us
 			this_flag->flag = true;
 			
 		}
-	}		
+	}
+
+UINT PeopleDetector::DealPeople(LPVOID pParam)
+{
+	PeopleDetector* p = (PeopleDetector*)pParam;
+	HOGDescriptor hog;  //HOG特征描述子
+	vector<Rect> found, found_filtered;
+	hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());  //得到SVM检测算子----用于对hog特征进行分类的svm模型的系数赋值
+	hog.detectMultiScale(p->img, found, 0, Size(8, 8), Size(0, 0), 1.35, 2);
+	
+	size_t i, j;
+	//找出没有嵌套的矩形框r，如果有嵌套的话，保留大的矩形框
+	for (i = 0; i < found.size(); i++)
+	{
+		Rect r = found[i];
+		for (j = 0; j < found.size(); j++)
+			if (j != i && (r & found[j]) == r)
+				break;
+		if (j == found.size())
+			found_filtered.push_back(r);
+	}
+	p->found_filtered = found_filtered;
+	p->found_filtered_flag = true;
+	return 0;
+}
+
+UINT PeopleDetector::NotifyPeople(LPVOID pParam)
+{
+	PeopleDetector* this_back = (PeopleDetector*)pParam;
+	int i = 0;
+	MessageBeep(MB_ICONHAND);
+	Sleep(2000);
+	
+	//this_back->flag = true;
+	return 0;
+}
 
 
 
